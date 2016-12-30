3f7f94c8af7166ce2fc4aa3ebf4eba6c84656c00
A.J.Rouvoet <arjen.rouvoet@stcorp.nl>
2016-12-30 23:59:46 +0100

    Fix protocol codec derivation

--- server/src/main/scala/io/airbender/protocol/protocol.scala
+++ server/src/main/scala/io/airbender/protocol/protocol.scala
@@ -8,16 +8,17 @@ import io.airbender.model._
 import io.airbender.applications._
 
 package protocol {
-  @JsonCodec case class Bar(i: Int, s: String)
-  sealed trait ClientMessage
+  @JsonCodec sealed trait ClientMessage
   case class KeyPress(keyCode: KeyCode) extends ClientMessage
-  case class Identify(username: String) extends ClientMessage
+  case class Identify(username: PlayerID) extends ClientMessage
+  object ClientMessage
 
-  case class ControlMessage(playerId: String, msg: ClientMessage)
+  @JsonCodec case class ControlMessage(playerId: String, msg: ClientMessage)
 
-  sealed trait ControlResponse
+  @JsonCodec sealed trait ControlResponse
   case class Identified(playerId: PlayerID) extends ControlResponse
   case class ClientError(message: String) extends ControlResponse
+  object ControlResponse
 }
 
 package object protocol {
